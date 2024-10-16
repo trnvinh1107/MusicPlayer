451fede7cf73c861d92ec9540b97b34afa06d6ff
trnvinh1107 <vinhtranthe117@gmail.com>
2024-10-16 16:51:31 +0700

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,70 +32,70 @@ const app = {
     {
       name: "Ánh nắng của anh",
       singer: "Đức Phúc",
-      path: "./music/AnhNangCuaAnh.mp3",
+      path: "./assets/music/AnhNangCuaAnh.mp3",
       image:
         "https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_jpeg/avatars/d/7/d7f34aa6b1112e4b605f6c6e7eccd162_1509437674.jpg",
     },
     {
       name: "Sau tất cả",
       singer: "Erik",
-      path: "./music/SauTatCa.mp3",
+      path: "./assets/music/SauTatCa.mp3",
       image:
         "https://photo-resize-zmp3.zmdcdn.me/w94_r1x1_jpeg/covers/5/5/55a2e33a5d4d6a70f5144181c28eacb0_1452855672.jpg",
     },
     {
       name: "Ôm trọn nỗi nhớ",
       singer: "Rum",
-      path: "./music/OmTronNoiNho.mp3",
+      path: "./assets/music/OmTronNoiNho.mp3",
       image:
         "https://photo-resize-zmp3.zmdcdn.me/w94_r1x1_jpeg/cover/0/c/6/6/0c6679e9039d45fd450147c768471457.jpg",
     },
     {
       name: "Chờ anh nhé",
       singer: "Hoàng Dũng",
-      path: "./music/ChoAnhNhe.mp3",
+      path: "./assets/music/ChoAnhNhe.mp3",
       image:
         "https://photo-resize-zmp3.zmdcdn.me/w94_r1x1_jpeg/covers/a/5/a5770da425db0bd4d1c397badb944075_1465443445.jpg",
     },
     {
       name: "Yêu em rất nhiều",
       singer: "Hoàng Tôn",
-      path: "./music/YeuEmRatNhieu.mp3",
+      path: "./assets/music/YeuEmRatNhieu.mp3",
       image:
         "https://photo-resize-zmp3.zmdcdn.me/w94_r1x1_jpeg/cover/6/8/0/7/6807770ea5dc38ceaaa3a70810d804b4.jpg",
     },
     {
       name: "Có hẹn với thanh xuân",
       singer: "MONSTAR & Grey D",
-      path: "./music/CoHenVoiThanhXuan.mp3",
+      path: "./assets/music/CoHenVoiThanhXuan.mp3",
       image:
         "https://photo-resize-zmp3.zmdcdn.me/w94_r1x1_jpeg/cover/e/2/3/f/e23ff2faaa64eebfc57e0acde247f0db.jpg",
     },
     {
       name: "Lần Hẹn Hò Đầu Tiên",
       singer: "Huyền Tâm Môn",
-      path: "./music/LanHenHoDauTien.mp3",
+      path: "./assets/music/LanHenHoDauTien.mp3",
       image:
         "https://photo-resize-zmp3.zmdcdn.me/w94_r1x1_jpeg/cover/7/e/9/4/7e94b95743998a108ff33beed5bae5c3.jpg",
     },
     {
       name: "Lệ Lưu Ly",
       singer: "Vũ Phụng Tiên & DT",
-      path: "./music/LeLuuLy.mp3",
+      path: "./assets/music/LeLuuLy.mp3",
       image:
         "https://photo-resize-zmp3.zmdcdn.me/w94_r1x1_jpeg/cover/6/9/3/f/693f8f516bfaa717ef4043f11edfdde2.jpg",
     },
     {
       name: "Mang Chủng / 芒种",
       singer: "Âm Khuyết Thi Thính",
-      path: "./music/MangChung.mp3",
+      path: "./assets/music/MangChung.mp3",
       image:
         "https://avatar-ex-swe.nixcdn.com/song/2019/08/05/1/9/9/6/1565016156395_500.jpg",
     },
     {
       name: "Đừng làm trái tim anh đau",
       singer: "Sơn Tùng M-TP",
-      path: "./music/DungLamTraiTimAnhDau.m4a",
+      path: "./assets/music/DungLamTraiTimAnhDau.m4a",
       image: "https://i.scdn.co/image/ab67616d00001e02a1bc26cdd8eecd89da3adc39",
     },
   ],
